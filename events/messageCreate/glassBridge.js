c6842e080c6daadb3e1b9979dfae12e97ed2270b
Omar Ahmed <livaiakramen4767@gmail.com>
2024-10-29 18:50:24 +0300

    uploaded new game

--- events/messageCreate/glassBridge.js
+++ events/messageCreate/glassBridge.js
@@ -10,7 +10,9 @@ const ROWS_PER_MESSAGE = 5; // Number of rows per message (Discord limit)
 const allowedChannels = [
   "1292642149493510184",
   "1277694414935953564",
-  "1290377082123194428"
+  "1290377082123194428",
+  "1300678838975729714",
+  "1294731828950732924"
 ];
 
 function createGlassPath(length) {
@@ -36,10 +38,12 @@ module.exports = {
 
       client.gamesStarted.set("glassBridge", true);
       const glassPath = createGlassPath(TOTAL_ROWS);
+      console.log(glassPath);
       const players = new Set();
       let currentPlayerIndex = 0;
       let currentRow = 0;
       let gameEnded = false;
+      let playersAttempts = new Map();
 
       try {
         const joinButton = new ButtonBuilder()
@@ -83,8 +87,8 @@ module.exports = {
             players.delete(interaction.user.id);
             // await interaction.reply({ content: `${interaction.user} غادر اللعبة!`, ephemeral: true });
           } else if (interaction.customId === 'start' && interaction.user.id === message.author.id) {
-            lobbyCollector.stop('gameStart');
             await interaction.reply('جاري بدء اللعبة...');
+            lobbyCollector.stop('gameStart');
             return;
           }
 
@@ -157,9 +161,18 @@ module.exports = {
           return [rows1, rows2];
         }
 
-        async function updateGameMessages(embed = null) {
+        async function updateGameMessages(embed = null,reason) {
           const [components1, components2] = createGameButtons();
           const updates = [];
+          if(reason == "allFailed" || reason == "timeout"){
+              updates.push(
+                gameMessage2.delete()
+              )
+              updates.push(
+                gameMessage1.edit({embeds:[embed],components:[]})
+              )
+              return;
+          }
           if (embed) {
             updates.push(gameMessage1.edit({ embeds: [embed], components: components1 }));
           } else {
@@ -205,23 +218,29 @@ module.exports = {
                     await playTurn(); // Continue with the same player
                 }
             } else {
+                if(currentRow === 0)currentRow++;
+                
                 if (response.message.id === gameMessage2.id) {
                     await response.update({ content: ` ` });
                     await gameMessage1.edit({ content: `💥 أوه لا! <@${currentPlayer}> سقط من الجسر!` });
                 } else {
                     await response.update({ content: `💥 أوه لا! <@${currentPlayer}> سقط من الجسر!` });
-                }              
-                players.delete(currentPlayer);
+                }
+                
+                if(playersAttempts.get(currentPlayer) >= 1) {
+                    players.delete(currentPlayer);
+                }
+                playersAttempts.set(currentPlayer, (playersAttempts.get(currentPlayer) || 0) + 1);
+
                 if (players.size === 0) {
                     await endGame('allFailed');
                 } else {
                     currentPlayerIndex = (currentPlayerIndex + 1) % players.size; // Move to the next player
-                    currentRow = 0; // Reset the row for the new player
+                    // currentRow = 0; // Reset the row for the new player
                     await playTurn();
                 }
             }
           } catch (error) {
-            console.log(error);
             const timeoutEmbed = new EmbedBuilder()
               .setColor('#ff0000')
               .setTitle('لعبة جسر الزجاج')
@@ -241,6 +260,7 @@ module.exports = {
         async function endGame(reason, winner = null) {
           if (gameEnded) return;
           gameEnded = true;
+        
           client.gamesStarted.set("glassBridge", false);
 
           const endEmbed = new EmbedBuilder()
@@ -264,19 +284,19 @@ module.exports = {
             const row = new ActionRowBuilder().addComponents(pointsButton);
             console.log("won")
             await message.channel.send({
-                content: `🏆 <@${winner}> فاز باللعبة وحصل على ${pointsEarned} `,
+                content: `🏆 <@${winner}> فاز باللعبة وحصل على ${pointsEarned} نقاط `,
                 components: [row],
             });
 
-          } else if (reason === 'allFailed') {
-            endEmbed.setDescription('انتهت اللعبة! جميع اللاعبين سقطوا من الجسر.');
-            if(gameMessage2) gameMessage2.delete();
-          } else if (reason === 'timeout') {
-            endEmbed.setDescription('انتهى وقت اللعبة! لم يتمكن أي لاعب من عبور الجسر بالكامل.');
-            if(gameMessage2) gameMessage2.delete();
           }
+          else if (reason === 'timeout') {
+            endEmbed.setDescription('انتهى وقت اللعبة! لم يتمكن أي لاعب من عبور الجسر بالكامل.');
+          }
+          else if (reason === 'allFailed') {
+            endEmbed.setDescription('انتهت اللعبة! جميع اللاعبين سقطوا من الجسر.');
+          } 
 
-          await updateGameMessages(endEmbed);
+          await updateGameMessages(endEmbed,reason);
         }
 
         // Set a timeout for the entire game
